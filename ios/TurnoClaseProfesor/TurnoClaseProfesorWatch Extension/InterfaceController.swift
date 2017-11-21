fe53d90bfc48d5604e4b2b638bea58a3becb3e14
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-11-21 14:27:47 +0100

    Interfaz de usuario completo

--- ios/TurnoClaseProfesor/TurnoClaseProfesorWatch Extension/InterfaceController.swift
+++ ios/TurnoClaseProfesor/TurnoClaseProfesorWatch Extension/InterfaceController.swift
@@ -12,17 +12,21 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var etiquetaAula: WKInterfaceLabel!
+    @IBOutlet var etiquetaNumero: WKInterfaceLabel!
+    @IBOutlet var etiquetaNombre: WKInterfaceLabel!
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+
         // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
