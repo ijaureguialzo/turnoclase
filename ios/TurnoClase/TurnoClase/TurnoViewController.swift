ff1d72e790479e0dd112db9139908ab7f139798b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-09-30 12:19:54 +0200

    Animar botones sólo si no estamos en modo test

--- ios/TurnoClase/TurnoClase/TurnoViewController.swift
+++ ios/TurnoClase/TurnoClase/TurnoViewController.swift
@@ -285,24 +285,32 @@ class TurnoViewController: UIViewController {
 
     @IBAction func fadeOut(_ sender: UIButton) {
 
-        // Difuminar
-        UIView.animate(withDuration: 0.1,
-            delay: 0,
-            options: UIView.AnimationOptions.curveLinear.intersection(.allowUserInteraction).intersection(.beginFromCurrentState),
-            animations: {
-                sender.alpha = 0.15
-            }, completion: nil)
+        // Ver si estamos en modo test, haciendo capturas de pantalla
+        if !UserDefaults.standard.bool(forKey: "FASTLANE_SNAPSHOT") {
+
+            // Difuminar
+            UIView.animate(withDuration: 0.1,
+                delay: 0,
+                options: UIView.AnimationOptions.curveLinear.intersection(UIView.AnimationOptions.allowUserInteraction).intersection(UIView.AnimationOptions.beginFromCurrentState),
+                animations: {
+                    sender.alpha = 0.15
+                }, completion: nil)
+        }
     }
 
     @IBAction func fadeIn(_ sender: UIButton) {
 
-        // Restaurar
-        UIView.animate(withDuration: 0.3,
-            delay: 0,
-            options: UIView.AnimationOptions.curveLinear.intersection(.allowUserInteraction).intersection(.beginFromCurrentState),
-            animations: {
-                sender.alpha = 1
-            }, completion: nil)
+        // Ver si estamos en modo test, haciendo capturas de pantalla
+        if !UserDefaults.standard.bool(forKey: "FASTLANE_SNAPSHOT") {
+
+            // Restaurar
+            UIView.animate(withDuration: 0.3,
+                delay: 0,
+                options: UIView.AnimationOptions.curveLinear.intersection(UIView.AnimationOptions.allowUserInteraction).intersection(UIView.AnimationOptions.beginFromCurrentState),
+                animations: {
+                    sender.alpha = 1
+                }, completion: nil)
+        }
     }
 
 }
